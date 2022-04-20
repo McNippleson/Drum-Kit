a0f1798150fc3b9870ab6a1ffa38ea988fd6a2a2
Artur L <falcon.arthur@gmail.com>
2022-04-20 17:11:59 +0300

    1.1

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        // let audio = new Audio('sounds/tom-1.mp3');
+        // audio.play();
+        console.log(this.innerHTML);
+        this.classList.toggle("clicked");
+        switch (key) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+        // document.querySelector("button").classList.toggle("pressed");
+
+    });
 }
 
 
-function handleClick() {
-    let audio = new Audio ('sounds/tom-1.mp3');
-    audio.play();
-    // document.querySelector("button").classList.toggle("pressed");
-
-}
